62024c541b7a038e20613161dbc870f9e02e53d4
Sarah Glotzbach <sarah.glotzbach@gmail.com>
2019-10-11 02:17:48 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -15,4 +15,5 @@ def swap_elements (array)
 end
 
 def swap_elements_from_to(array,index,destination_index)
-  array
+  array[index],array[destination_index] = array[destination_index],array[index]
+end
