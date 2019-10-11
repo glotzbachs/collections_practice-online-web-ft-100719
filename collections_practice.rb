4a8f13cb00eba2bf33c246b8a72c035fc42c5ef5
Sarah Glotzbach <sarah.glotzbach@gmail.com>
2019-10-11 02:50:19 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -23,8 +23,7 @@ def kesha_maker(array)
 end
 
 def find_a(array)
- a=[]
- a<< array.find_all {|word| word.start_with?("a")}
+ array.find_all {|word| word.start_with?("a")}
 end
 
 def sum_array(array)
