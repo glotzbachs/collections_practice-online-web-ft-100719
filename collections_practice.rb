4130bd1cca23d45883a13cd77ccda866da37ae8d
Sarah Glotzbach <sarah.glotzbach@gmail.com>
2019-10-11 02:18:54 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -14,6 +14,7 @@ def swap_elements (array)
   array[1], array[2] = array[2], array[1]
 end
 
-def swap_elements_from_to(array,index,destination_index)
-  array[index],array[destination_index] = array[destination_index],array[index]
-end
+#ADVANCED SWAP
+# def swap_elements_from_to(array,index,destination_index)
+#   array[index],array[destination_index] = array[destination_index],array[index]
+# end
