3909e79322484d640e596ba7def264bc73947c13
Sarah Glotzbach <sarah.glotzbach@gmail.com>
2019-10-11 02:21:44 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -11,7 +11,7 @@ def sort_array_char_count(array)
 end
 
 def swap_elements (array)
-  array.sort{array[1], array[2] = array[2], array[1]}
+  array.sort{|a=1,b=2| array[a], array[b] = array[b], array[a]}
 end
 
 #ADVANCED SWAP
